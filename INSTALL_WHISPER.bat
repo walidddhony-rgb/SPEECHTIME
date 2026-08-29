838ca6be39f73043ae32dfa03c159cfa9d175eee
walidddhony-rgb <walidddhony@gmail.com>
2026-08-29 22:19:27 +0300

    Delete INSTALL_WHISPER.bat

--- INSTALL_WHISPER.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-chcp 65001 >nul
-cd /d "%~dp0"
-echo Installing local Whisper dependencies...
-py -m pip install --upgrade pip
-py -m pip install -r requirements-whisper.txt
-if errorlevel 1 (echo Installation failed.& pause & exit /b 1)
-py -c "from faster_whisper import WhisperModel; print('faster-whisper installed successfully.')"
-pause
