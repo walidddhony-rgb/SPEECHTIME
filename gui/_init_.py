2250e9969f71368a489efff49bf3a0331e8feacd
walidddhony-rgb <walidddhony@gmail.com>
2026-08-29 22:22:17 +0300

    Delete gui/_init_.py

--- gui/_init_.py
+++
@@ -1,9 +0,0 @@
-"""
-GUI module for SpeechScribe.
-Professional graphical interface for semi-automatic transcription.
-"""
-
-from .main_window import SpeechScribeMainWindow
-
-__version__ = "1.0.0"
-__all__ = ["SpeechScribeMainWindow"]
